aa1b221b90e5bbf5be5b4630709fdd5bab32861a
mrmin50000 <danilast2007@gmail.com>
2024-10-18 13:58:30 +0300

    theme

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,39 @@
 fun main(){
-    println(123)
+    val student: Student? = Student()
 
+    val student1 = student?.let{ st -> //не возвращает обратного значения, рабтаем через ключевое слово
+        st.hi()
+        st.bye()
+        st.name = "Masha"
+    }
+
+    val student2 = student?.apply{ //возвращает измененный объект, работаем с содержимым объекта
+        hi()
+        bye()
+        name = "LklKek"
+    }
+
+    val count = Student().run{ //возвращает обратное значение с посл. строки, работаем с содержимым объекта
+        hi()
+        bye()
+        countOfDebts = 15
+        countOfDebts
+    }
+
+    val str = with(Student()){
+        hi()
+        bye()
+        name = "Pasha"
+        name
+    }
+    println(str)
+
+    val student3 = Student().also{ st -> //возвращает измененный объект, работаем через ключевое слово
+
+        st.hi()
+        st.bye()
+        st.countOfDebts = 3
+    }
 
 
 }
